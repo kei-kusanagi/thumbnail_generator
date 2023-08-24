9c4c8e3d08b21ea4d90a910aebe84e9d937e345f
kei-kusanagi <kei_kusanagi@outlook.com>
2023-08-23 20:17:13 -0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:thumblr/thumblr.dart';
+import 'dart:io';
 
 void main() {
   runApp(const MyApp());
@@ -18,26 +21,39 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-    getThumbnail();
   }
 
-  Future<void> getThumbnail() async {
+  Future<void> saveImageToFile(Uint8List imageBytes) async {
+    Uint8List imageBytes = await thumb!.image
+        .toByteData(format: ImageByteFormat.png)
+        .then((byteData) => byteData!.buffer.asUint8List());
+
+    String tempDir = Directory.systemTemp.path;
+    File imageFile = File('$tempDir/thumbnail.png');
+    await imageFile.writeAsBytes(imageBytes);
+
+    debugPrint('Image saved in: ${imageFile.path}');
+  }
+
+  getThumbnail(TextEditingController urlController) async {
     try {
       thumb = await generateThumbnail(
-        filePath:
-            "C:/Users/admin/Desktop/Proyectos/Flutter/app_code_test/assets/Genshin.mp4",
+        filePath: urlController.text,
         position: 0.5,
       );
+
+      setState(() {});
     } on PlatformException catch (e) {
       debugPrint('Failed to generate thumbnail: ${e.message}');
     } catch (e) {
       debugPrint('Failed to generate thumbnail: ${e.toString()}');
     }
-    debugPrint(thumb.toString());
   }
 
   @override
   Widget build(BuildContext context) {
+    final TextEditingController urlController = TextEditingController();
+
     return MaterialApp(
       theme: ThemeData(brightness: Brightness.dark),
       home: Material(
@@ -46,10 +62,26 @@ class _MyAppState extends State<MyApp> {
             Expanded(
               child: Padding(
                 padding: const EdgeInsets.all(16.0),
-                child: Center(
-                  child: thumb?.image != null
-                      ? RawImage(image: thumb!.image)
-                      : const Placeholder(),
+                child: Column(
+                  children: [
+                    Center(
+                      child: thumb?.image != null
+                          ? RawImage(image: thumb!.image)
+                          : const Placeholder(),
+                    ),
+                    TextField(
+                      controller: urlController,
+                      decoration: const InputDecoration(
+                        hintText: 'Enter video URL',
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        getThumbnail(urlController);
+                      },
+                      child: const Text('Save thumbnail'),
+                    ),
+                  ],
                 ),
               ),
             ),
